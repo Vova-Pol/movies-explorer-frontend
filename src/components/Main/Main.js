873e5b20a4cde409a09fecc45ec236910e19602f
Vova Pol <ya-chai777@yandex.ru>
2023-02-28 15:51:36 +0300

    сделал верстку компонента AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,18 @@ import './Main.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(true);
+
+  // --- Компоненту NavTab прописать якорные ссылки
   return (
     <div className="main">
       <Header loggedIn={loggedIn} />
       <Promo />
       <NavTab />
+      <AboutProject />
     </div>
   );
 }
