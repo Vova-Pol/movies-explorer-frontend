98fc16619c8251a5e1d7f58253284f989b9beb1d
Vova Pol <ya-chai777@yandex.ru>
2023-02-28 13:17:12 +0300

    сверстал компонент Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './Main.css';
 import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(true);
   return (
     <div className="main">
       <Header loggedIn={loggedIn} />
+      <Promo />
     </div>
   );
 }
