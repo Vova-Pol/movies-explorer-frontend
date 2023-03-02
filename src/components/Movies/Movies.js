1b90a1a62bcd280dd6ba17f3667f4296a7d6a320
Vova Pol <ya-chai777@yandex.ru>
2023-03-02 17:41:21 +0300

    сделал черновую верстку компонента Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,6 +2,7 @@ import './Movies.css';
 import { useState } from 'react';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 import Footer from '../Footer/Footer';
 
 function Movies() {
@@ -11,6 +12,7 @@ function Movies() {
     <div className="movies">
       <Header loggedIn={loggedIn} />
       <SearchForm />
+      <Preloader />
       <Footer />
     </div>
   );
