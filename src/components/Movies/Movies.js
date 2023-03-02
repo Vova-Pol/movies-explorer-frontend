f79e9364f92663163feac1b6abb49554622b7434
Vova Pol <ya-chai777@yandex.ru>
2023-03-02 19:37:32 +0300

    сделал верстку компонента More, поправил верстку MoviesCard

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -5,6 +5,7 @@ import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
+import More from '../More/More';
 
 function Movies() {
   const [loggedIn, setLoggedIn] = useState(true);
@@ -15,6 +16,7 @@ function Movies() {
       <SearchForm />
       <Preloader />
       <MoviesCardList />
+      <More />
       <Footer />
     </div>
   );
