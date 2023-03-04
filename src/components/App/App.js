3259321c5695f3cdf1b0cd40e9df8f58e69bedcf
Vova Pol <ya-chai777@yandex.ru>
2023-03-04 19:13:01 +0300

    сделал верстку компонента NotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,17 @@ import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import Register from '../Register/Register';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <div className="app">
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
       {/* <Main /> */}
       {/* <Movies /> */}
       {/* <SavedMovies /> */}
+      <NotFound />
     </div>
   );
 }
