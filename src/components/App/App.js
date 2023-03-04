6e8e78e6b0ba27e5feb16ba1b5a0ec943ebcd51e
Vova Pol <ya-chai777@yandex.ru>
2023-03-04 14:39:19 +0300

    вынес общие стили для Register и Login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import '../../index.css';
+import Login from '../Login/Login';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import Register from '../Register/Register';
@@ -7,6 +8,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 function App() {
   return (
     <div className="app">
+      {/* <Login /> */}
       <Register />
       {/* <Main /> */}
       {/* <Movies /> */}
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,39 +1,51 @@
-import './Register.css';
+import '../App/App.css';
 import { Link } from 'react-router-dom';
 
 function Register() {
   return (
-    <div className="register">
-      <div className="register__top">
-        <div className="register__logo"></div>
-        <h1 className="register__title">Добро пожаловать!</h1>
+    <div className="login-register">
+      <div className="login-register__top">
+        <div className="login-register__logo"></div>
+        <h1 className="login-register__title">Добро пожаловать!</h1>
       </div>
-      <form className="register__form">
-        <label for="name" className="register__label">
+      <form className="login-register__form">
+        <label for="name" className="login-register__label">
           Имя
         </label>
-        <input type="text" name="name" className="register__input"></input>
-        <span className="register__error-text"></span>
-        <label for="email" className="register__label">
+        <input
+          type="text"
+          name="name"
+          className="login-register__input"
+        ></input>
+        <span className="login-register__error-text"></span>
+        <label for="email" className="login-register__label">
           E-mail
         </label>
-        <input type="email" name="email" className="register__input"></input>
-        <span className="register__error-text"></span>
-        <label for="password" className="register__label">
+        <input
+          type="email"
+          name="email"
+          className="login-register__input"
+        ></input>
+        <span className="login-register__error-text"></span>
+        <label for="password" className="login-register__label">
           Пароль
         </label>
         <input
           type="password"
           name="password"
-          className="register__input"
+          className="login-register__input"
         ></input>
-        <span className="register__error-text">Что-то пошло не так...</span>
-        <button type="submit" className="register__submit-btn">
+        <span className="login-register__error-text">
+          Что-то пошло не так...
+        </span>
+        <button type="submit" className="login-register__submit-btn">
           Зарегистрироваться
         </button>
-        <div className="register__login-suggest-container">
-          <span className="register__login-suggest">Уже зарегистрированы?</span>
-          <Link to="/signin" className="register__link">
+        <div className="login-register__login-suggest-container">
+          <span className="login-register__login-suggest">
+            Уже зарегистрированы?
+          </span>
+          <Link to="/signin" className="login-register__link">
             Войти
           </Link>
         </div>
