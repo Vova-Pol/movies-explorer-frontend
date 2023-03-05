c158b7dc1485efdb2c5678f468a9295afb6f913b
Vova Pol <ya-chai777@yandex.ru>
2023-03-05 20:01:43 +0300

    сверстал иконку мобтльного меню и его анимацию

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,7 +1,12 @@
 import './MobileMenu.css';
 
 function MobileMenu() {
-  return <div className="mobile-menu"></div>;
+  return (
+    <div className="mobile-menu">
+      <input type="checkbox" className="mobile-menu__toggle"></input>
+      <div className="mobile-menu__icon"></div>
+    </div>
+  );
 }
 
 export default MobileMenu;
