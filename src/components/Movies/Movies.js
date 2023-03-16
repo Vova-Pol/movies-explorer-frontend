40b03bb20aed25bd3f191dd2061840a82817d73a
Vova Pol <ya-chai777@yandex.ru>
2023-03-16 15:35:13 +0300

    написал загрузку фильмов по 5 и по 7шт в зависимости от разрешения экрана. Поправил верстку прелоудера

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,6 @@
 import './Movies.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import useResize from '../../hooks/useResize';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
@@ -14,7 +15,19 @@ function Movies() {
   const [isLoading, setIsLoading] = useState(false);
   const [isNothingFound, setIsNothingFound] = useState(false);
   const [isServerError, setIsServerError] = useState(false);
-  const [moviesAmount, setMoviesAmount] = useState(7);
+  const [moviesAmount, setMoviesAmount] = useState(0);
+  const [moviesAmountStep, setMoviesAmountStep] = useState(0);
+  const { screenWidth, isScreenLaptop, isScreenMobile } = useResize();
+
+  useEffect(() => {
+    if (isScreenLaptop) {
+      setMoviesAmount(7);
+      setMoviesAmountStep(7);
+    } else if (isScreenMobile) {
+      setMoviesAmount(5);
+      setMoviesAmountStep(5);
+    }
+  }, []);
 
   async function handleSearchForm(values) {
     try {
@@ -40,7 +53,7 @@ function Movies() {
   }
 
   function handleMoreButton() {
-    setMoviesAmount(moviesAmount + 7);
+    setMoviesAmount(moviesAmount + moviesAmountStep);
   }
 
   return (
