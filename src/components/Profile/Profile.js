347d115f2c6cbaccf90419d2b37c0a4aa0129ef4
Vova Pol <ya-chai777@yandex.ru>
2023-03-23 17:29:56 +0300

    настроил отображение данных в компоненте Profile с использованием CurrentUserContext

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,23 +1,25 @@
 import './Profile.css';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Header from '../Header/Header';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function Profile() {
   const [loggedIn, setLoggedIn] = useState(true);
+  const currentUser = useContext(CurrentUserContext);
   return (
     <div className="profile">
       <Header loggedIn={loggedIn} />
       <main>
         <div className="profile__container">
-          <h1 className="profile__title">Привет, Виталий!</h1>
+          <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
           <div className="profile__info-container">
             <p className="profile__info-item">Имя</p>
-            <p className="profile__info-item">Виталий</p>
+            <p className="profile__info-item">{currentUser.name}</p>
           </div>
           <div className="profile__line"></div>
           <div className="profile__info-container">
             <p className="profile__info-item">E-mail</p>
-            <p className="profile__info-item">pochta@yandex.ru</p>
+            <p className="profile__info-item">{currentUser.email}</p>
           </div>
           <button type="button" className="profile__edit-button">
             Редактировать
