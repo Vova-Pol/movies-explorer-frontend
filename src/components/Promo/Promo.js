fab20e29d64e2318b752f7947a0888df65318c6b
Vova Pol <ya-chai777@yandex.ru>
2023-09-25 17:14:16 +0300

    Правки в компоненте Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,12 +1,11 @@
 import './Promo.css';
-import { ReactComponent as BackgroundIcon } from '../../images/background-icon.svg';
+import { ReactComponent as BackgroundIcon } from '../../images/promo-background.svg';
 
 function Promo() {
   return (
     <section className="promo">
-      <h1 className="promo__title">
-        Учебный проект студента факультета Веб-разработки.
-      </h1>
+      <h1 className="promo__title">Movie Explorer</h1>
+      <h2 className="promo__subtitle">Найди фильм по душе</h2>
       <BackgroundIcon className="promo__background" />
     </section>
   );
