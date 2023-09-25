f571eb86535c9fe6a6c0a607586c88f0e99ef3f0
Vova Pol <ya-chai777@yandex.ru>
2023-09-25 17:21:21 +0300

    Удалены лишние компоненты из Main Page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,6 @@ import { useState } from 'react';
 import './Main.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
-import NavTab from '../NavTab/NavTab';
-import AboutProject from '../AboutProject/AboutProject';
-import Techs from '../Techs/Techs';
-import AboutMe from '../AboutMe/AboutMe';
 import Footer from '../Footer/Footer';
 
 function Main(props) {
@@ -14,10 +10,6 @@ function Main(props) {
       <Header loggedIn={props.loggedIn} />
       <main>
         <Promo />
-        <NavTab />
-        <AboutProject />
-        <Techs />
-        <AboutMe />
       </main>
       <Footer />
     </div>
