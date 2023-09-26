59a2fb6f4fa0cd1a0f99343b04f8489b2659c7de
Vova Pol <ya-chai777@yandex.ru>
2023-09-26 17:36:40 +0300

    Правки верстки карточки фильма

--- src/components/PreviewMovieCard/PreviewMovieCard.js
+++ src/components/PreviewMovieCard/PreviewMovieCard.js
@@ -10,6 +10,10 @@ export const PreviewMovieCard = ({ card }) => {
         className="preview-movie-card__thumbnail"
         src={`${IMAGES_URL}${card.image.formats.thumbnail.url}`}
       />
+      <div className="preview-movie-card__info">
+        <p className="preview-movie-card__title">{card.nameRU}</p>
+        <p className="preview-movie-card__year">{card.year}</p>
+      </div>
     </div>
   );
 };
