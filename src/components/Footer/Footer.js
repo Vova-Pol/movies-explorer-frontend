b2e221515dff2e1dbf6274e417798b4d526665fd
Vova Pol <ya-chai777@yandex.ru>
2023-09-29 11:32:49 +0300

    Отредактирован футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,34 @@
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   const currentDate = new Date();
 
   return (
     <footer className="footer">
-      <p className="footer__text">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </p>
+      <p className="footer__text">Демонстрационный Full Stack проект</p>
       <div className="footer__line"></div>
       <div className="footer__container">
         <p className="footer__date">{currentDate.getFullYear()}</p>
         <div className="footer__links-container">
-          <p className="footer__link">Яндекс.Практикум</p>
-          <p className="footer__link">Github</p>
+          <Link
+            className="footer__link"
+            to="https://github.com/Vova-Pol/movies-explorer-frontend"
+          >
+            Документация
+          </Link>
+          <Link
+            className="footer__link"
+            to="https://github.com/Vova-Pol/movies-explorer-frontend"
+          >
+            Технологии
+          </Link>
+          <Link
+            className="footer__link"
+            to="https://github.com/Vova-Pol/movies-explorer-frontend"
+          >
+            Github
+          </Link>
         </div>
       </div>
     </footer>
