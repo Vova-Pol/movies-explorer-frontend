1b4317f61fcc9229d6751bd508a9e45684ac811d
Vova Pol <ya-chai777@yandex.ru>
2023-10-01 11:27:56 +0300

    index перенесен на typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
   <BrowserRouter>
     <React.StrictMode>
