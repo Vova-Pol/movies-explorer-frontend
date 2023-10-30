2e561b671c2a20d2198d3e09f63e90fcdb5b097f
Vova Pol <ya-chai777@yandex.ru>
2023-10-30 14:02:14 +0300

    Написана функция высчита возраста по дате

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,11 @@
-function countDuration(num: number): string {
+export function countDuration(num: number): string {
   const hours = Math.floor(num / 60);
   const minutes = num % 60;
   return `${hours}ч ${minutes}м`;
 }
 
-export default countDuration;
+export function countUserAge(dateOfBirth: string): string {
+  const now = new Date().valueOf();
+  const birth = new Date(dateOfBirth).valueOf();
+  return String(new Date(now - birth).getFullYear() - 1970);
+}
