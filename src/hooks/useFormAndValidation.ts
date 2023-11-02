ba0a25f3e4b3395cd6ef62c93c8cc6fd5eba6b5c
Vova Pol <ya-chai777@yandex.ru>
2023-11-02 18:51:57 +0300

    Доработан хук useFormAndValidation

--- src/hooks/useFormAndValidation.ts
+++ src/hooks/useFormAndValidation.ts
@@ -1,15 +1,22 @@
 import React, { ChangeEvent } from 'react';
 
-export function useFormAndValidation<T>(initialInputValues: T) {
-  const [values, setValues] = React.useState(initialInputValues);
+export function useFormAndValidation<T extends Object>(initialInputValues: T) {
+  const [values, setValues] = React.useState<T>(initialInputValues);
   const [errors, setErrors] = React.useState(initialInputValues);
-  const [isValid, setIsValid] = React.useState(false);
+  const [isValid, setIsValid] = React.useState(true);
+  const [isValuesChanged, setIsValuesChanged] = React.useState(
+    JSON.stringify(initialInputValues) !== JSON.stringify(values),
+  );
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = evt.target;
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: evt.target.validationMessage });
     setIsValid(evt.target.closest('form')!.checkValidity());
+    setIsValuesChanged(
+      JSON.stringify(initialInputValues) !==
+        JSON.stringify({ ...values, [name]: value }),
+    );
   };
 
   const resetForm = () => {
@@ -25,5 +32,7 @@ export function useFormAndValidation<T>(initialInputValues: T) {
     errors,
     isValid,
     resetForm,
+    setIsValuesChanged,
+    isValuesChanged,
   };
 }
