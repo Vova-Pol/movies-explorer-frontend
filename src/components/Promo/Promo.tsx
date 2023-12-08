ee28c1df2d8758b4ad9c29283246345618ffbad7
Vova Pol <ya-chai777@yandex.ru>
2023-12-08 10:11:17 +0300

    Правки по верстке

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -6,7 +6,7 @@ import { FC } from 'react';
 const Promo: FC = () => {
   return (
     <section className="promo">
-      <h1 className="promo__title">Movie Explorer</h1>
+      <h1 className="promo__title">Movies Explorer</h1>
       <h2 className="promo__subtitle">Найди фильм по душе</h2>
       <BiMoviePlay className="promo__background" />
     </section>
